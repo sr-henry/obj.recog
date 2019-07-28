ff3a30ea24bc0443c87e7d7ec88d0105841a4675
sr-henry <37659078+sr-henry@users.noreply.github.com>
2019-07-28 18:56:23 -0300

    update

--- obj_detection/shape detection/shape.py
+++ obj_detection/shape detection/shape.py
@@ -10,21 +10,21 @@ def compare_imgs(i1, i2, hausdorff = False):
     distances_1 = compute_distance(coords1, coords2)
     distances_2 = compute_distance(coords2, coords1)
     degree = similarity_degree(distances_1, distances_2)
-    H = max(max(distances_1), max(distances_2))
     if hausdorff:
+        H = max(max(distances_1), max(distances_2))
         return degree, H
     return degree
 
 
 def funct(im, imgs):
-    return list(map(lambda m: compare_imgs(im, m), imgs))
+    return np.array(list(map(lambda m: compare_imgs(im, m), imgs)))
 
 
 def compare_sub(matrix1, matrix2):
     section = 2
     f_imgs1 = get_sub_imgs(matrix1, section)
     f_imgs2 = get_sub_imgs(matrix2, section)
-    return list(map(lambda img: funct(img, f_imgs2), f_imgs1))
+    return np.array(list(map(lambda img: funct(img, f_imgs2), f_imgs1)))
 
 
 def get_sub_imgs(matrix, section):
@@ -42,16 +42,9 @@ def rgb_2_int(rgb):
     return rgb_int
 
 
-def select_img(matrix):
-    x, y = np.where(matrix == 0)
-    selected = matrix[min(x):max(x), min(y):max(y)]
-    return selected
-
-
 def image_2_matrix(im):
     base = np.asarray(im).reshape(400, 3)
     mapped = np.array(list(map(rgb_2_int, base))).reshape((20, 20))
-    mapped[np.where(mapped != 0)] = 1
     return mapped
 
 
@@ -62,11 +55,8 @@ def get_coords(matrix):
 
 
 def distance(array, point):
-    diff = array - point
-    dx = diff[:, 0]
-    dy = diff[:, 1]
-    euclidean = np.sqrt((dx**2 + dy**2))
-    return min(euclidean)
+    euclidean_distance = np.linalg.norm((array - point), axis=1)
+    return np.ndarray.min(euclidean_distance)
 
 
 def compute_distance(c1, c2):
@@ -76,11 +66,11 @@ def compute_distance(c1, c2):
 def similarity_degree(values1, values2):
     d1 = np.average(values1)
     d2 = np.average(values2)
-    #std1 = np.std(values1)
-    #std2 = np.std(values2)
-    #print('\n[1] M(A, B): ' + str(d1) + '\tstd: ' + str(std1))
-    #print('[2] M(B, A): ' + str(d2) + '\tstd: ' + str(std2))
-    return (d1 + d2) / 2
+    std1 = np.std(values1)
+    std2 = np.std(values2)
+    print('\n[1] M(A, B): ' + str(d1) + '\tstd: ' + str(std1))
+    print('[2] M(B, A): ' + str(d2) + '\tstd: ' + str(std2))
+    return np.average([d1, d2])
 
 
 img_name = sys.argv[1]
@@ -90,25 +80,24 @@ start = time.time()
 matrix_base = image_2_matrix(Image.open("base.png"))
 matrix_cptr = image_2_matrix(Image.open(img_name))
 
-base = matrix_base#select_img(matrix_base)
-cptr = matrix_cptr#select_img(matrix_cptr)
 
 # ======================================================================
-dist_matrix = np.array(compare_sub(matrix_base, matrix_cptr))
+dist_matrix = compare_sub(matrix_base, matrix_cptr)
 print('\n' + str(dist_matrix) + '\n')
 
-minimum = np.array(list(map(lambda el: min(el), dist_matrix)))
+minimum = np.array(list(map(lambda el: np.ndarray.min(el), dist_matrix)))
 #minimum = list(filter(lambda x: x <= 1.35, minimum))
-print(minimum)
-diagonal = np.array(list(filter(lambda el: el <= 1.35, dist_matrix.diagonal())))
-print(diagonal)
+print('minimum per line:' + str(minimum))
+diagonal = dist_matrix.diagonal()
+print('diagonal: ' + str(diagonal))
 
-print(np.average(diagonal))
+print('\ndiagonal_mean: ' + str(np.average(diagonal)))
 
 print('____________________________________________________')
 # ======================================================================
 
-degree, H = compare_imgs(base, cptr, True)
+
+degree, H = compare_imgs(matrix_base, matrix_cptr, True)
 
 print('\naverage: ' + str(degree) + '\n')
 
@@ -118,9 +107,8 @@ if degree <= 1.35 and H <= 4:
     print('similar images')
 else:
     print('non-similar images')
-    if H < 5:
-        print('\nLet me check again...')
-    
+
+
 end = time.time()
 
 print('\ntime: ' + str(end - start))
